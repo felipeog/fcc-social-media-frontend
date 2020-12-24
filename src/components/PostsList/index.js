3d2ab06504bacf9046cf41d702b2eebf6b60ed50
felipeog <felipeog476@gmail.com>
2020-12-24 18:37:49 -0300

    Add transition to posts list

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Transition } from 'semantic-ui-react'
 
 import PostCard from '../PostCard'
 
@@ -7,11 +7,13 @@ const PostsList = ({ posts }) => {
   return (
     <Grid className="PostsList" columns={3}>
       <Grid.Row>
-        {posts.map((post) => (
-          <Grid.Column key={post.id}>
-            <PostCard post={post} />
-          </Grid.Column>
-        ))}
+        <Transition.Group>
+          {posts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))}
+        </Transition.Group>
       </Grid.Row>
     </Grid>
   )
