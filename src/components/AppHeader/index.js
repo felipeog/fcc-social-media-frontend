78d3245f935c55c19c5f6ce4290eb62622709c26
felipeog <felipeog476@gmail.com>
2020-12-24 13:11:25 -0300

    Add logout button when logged in

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom'
 
+import { UserContext } from '../../context/User'
+
 const AppHeader = () => {
+  // hooks
+  const { user, logout } = useContext(UserContext)
+
+  // rendering
+  const renderLeftSide = () => {
+    if (user) {
+      return (
+        <Menu.Item
+          name={user.username}
+          isActive={() => true}
+          as={NavLink}
+          to="/home"
+        />
+      )
+    } else {
+      return <Menu.Item name="home" as={NavLink} to="/home" />
+    }
+  }
+  const renderRightSide = () => {
+    if (user) {
+      return (
+        <>
+          <Menu.Item name="logout" onClick={logout} />
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Menu.Item name="login" as={NavLink} to="/login" />
+          <Menu.Item name="register" as={NavLink} to="/register" />
+        </>
+      )
+    }
+  }
+
   return (
     <Menu pointing secondary>
-      <Menu.Item name="home" as={NavLink} to="/home" />
+      {renderLeftSide()}
 
-      <Menu.Menu position="right">
-        <Menu.Item name="login" as={NavLink} to="/login" />
-        <Menu.Item name="register" as={NavLink} to="/register" />
-      </Menu.Menu>
+      <Menu.Menu position="right">{renderRightSide()}</Menu.Menu>
     </Menu>
   )
 }
