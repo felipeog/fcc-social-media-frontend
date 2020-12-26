9ac3e3350a26991d9c3d94711978df2df14bc260
felipeog <felipeog476@gmail.com>
2020-12-26 14:12:30 -0300

    Add post card to single post page

--- src/pages/SinglePost/index.js
+++ src/pages/SinglePost/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
-import { Loader, Grid, Card } from 'semantic-ui-react'
+import { Loader } from 'semantic-ui-react'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
-import LikePostButton from '../../components/LikePostButton'
-import CommentPostButton from '../../components/CommentPostButton'
-import DeletePostButton from '../../components/DeletePostButton'
 import CommentsList from '../../components/CommentsList'
+import PostCard from '../../components/PostCard'
 import { POST_QUERY } from './query'
 
 dayjs.extend(relativeTime)
@@ -39,42 +37,11 @@ const SinglePost = ({
     const post = data?.getPost
     if (!post) return <h1>No post found</h1>
 
-    const {
-      body,
-      commentCount,
-      createdAt,
-      id,
-      likeCount,
-      username,
-      likes,
-      comments,
-    } = post
+    const { id, comments } = post
 
     return (
       <>
-        <Grid>
-          <Grid.Row>
-            <Card fluid>
-              <Card.Content>
-                <Card.Header>{username}</Card.Header>
-                <Card.Meta>{dayjs(createdAt).fromNow(true)}</Card.Meta>
-                <Card.Description>{body}</Card.Description>
-              </Card.Content>
-
-              <Card.Content extra>
-                <LikePostButton post={{ id, likes, likeCount }} />
-
-                <CommentPostButton post={{ id, commentCount }} />
-
-                <DeletePostButton
-                  post={{ id, username }}
-                  callback={onDeleteCallback}
-                />
-              </Card.Content>
-            </Card>
-          </Grid.Row>
-        </Grid>
-
+        <PostCard post={post} onDeleteCallback={onDeleteCallback} />
         <CommentsList postId={id} comments={comments} />
       </>
     )
