67960a4fc77f802f8458e481e16cafed9aae875b
felipeog <felipeog476@gmail.com>
2020-12-27 21:38:23 -0300

    Add Apollo erro link

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,13 +5,11 @@ import {
   ApolloClient,
   ApolloProvider,
 } from '@apollo/client'
-import { setContext } from 'apollo-link-context'
+import { ApolloLink } from '@apollo/client/link/core'
+import { setContext } from '@apollo/client/link/context'
+import { onError } from '@apollo/client/link/error'
 
 import { LS_TOKEN_KEY } from './consts'
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
-})
 
 const authLink = setContext(() => {
   const token = localStorage.getItem(LS_TOKEN_KEY)
@@ -23,9 +21,32 @@ const authLink = setContext(() => {
   }
 })
 
+const stringify = (input) => JSON.stringify(input, null, 2)
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path, extensions: { code } }) => {
+      console.error('[GraphQL error]')
+      console.group()
+      console.error(`Message: ${stringify(message)}`)
+      console.error(`Code: ${stringify(code)}`)
+      console.error(`Operation: ${stringify(operation.operationName)}`)
+      console.error(`Locations: ${stringify(locations)}`)
+      console.error(`Path: ${stringify(path)}`)
+      console.groupEnd()
+    })
+
+  if (networkError) console.error(`[Network error]: ${stringify(networkError)}`)
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000',
+})
+
+const link = ApolloLink.from([authLink, errorLink, httpLink])
+
 const mergeIncoming = (_, incoming) => incoming
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
