4a4525cdde2965fb86ad40131c632d66a681793e
felipeog <felipeog476@gmail.com>
2020-12-28 02:23:30 -0300

    Cleanup

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Error404 = () => {
   return (
-    <>
+    <div className="Error404">
       <h1>404</h1>
       <h2>Page not found</h2>
-    </>
+    </div>
   )
 }
 
